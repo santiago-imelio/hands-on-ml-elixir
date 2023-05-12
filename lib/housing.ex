a719ce705b9a5c6d36273cfc4ad1dc2e5a176205
Santiago Imelio <santiago.imelio@mimiquate.com>
2023-05-12 19:16:12 -0300

    add income category and plot

--- lib/housing.ex
+++ lib/housing.ex
@@ -1,6 +1,8 @@
 defmodule Learning.Housing do
   alias Explorer.DataFrame, as: DF
+  alias Explorer.Series, as: S
 
+  # Fetch housing data that we'll use to train our model
   def load_housing_data do
     url = "https://raw.githubusercontent.com/ageron/data/main/housing.tgz"
     {:ok, %Req.Response{status: 200, body: raw_data}} = Req.get(url)
@@ -22,6 +24,8 @@ defmodule Learning.Housing do
     housing_df
   end
 
+  # Randomly partition our data into two, sets: training data set that we'll
+  # use to train the model; and test data set that we'll use to test it.
   def shuffle_and_split_data(dataframe, test_ratio \\ 0.20) do
     shuffled_data = DF.shuffle(dataframe, seed: 42)
     total_data_size = DF.n_rows(dataframe)
@@ -30,5 +34,22 @@ defmodule Learning.Housing do
     train_data = DF.tail(shuffled_data, total_data_size - test_data_size)
 
     %{"train" => train_data, "test" => test_data}
+  end
+
+  def add_income_category_column(housing_df) do
+    income_cat =
+      housing_df
+      |> DF.pull("median_income")
+      |> S.transform(&bin_median_income/1)
+
+    DF.put(housing_df, :income_category, income_cat)
+  end
+
+  defp bin_median_income(income_value) do
+    bins = [0, 1.5, 3.0, 4.5, 6.0, :infinity]
+
+    Enum.find_index(bins, fn bin ->
+      income_value <= bin
+    end)
   end
 end
--- lib/housing/plot.ex
+++ lib/housing/plot.ex
@@ -2,7 +2,7 @@ defmodule Learning.Housing.Plot do
   alias VegaLite, as: VL
 
   # Taken from https://hexdocs.pm/scholar/linear_regression.html#california-housing
-  def all_features_hist(df)  do
+  def all_features_hist(df) do
     VL.new(
       title: [
         text: "Univariate Histograms of all features"
@@ -26,6 +26,28 @@ defmodule Learning.Housing.Plot do
         |> VL.encode_field(:y, "value count", aggregate: :count)
       end
     )
+    |> VL.Viewer.show()
+  end
+
+  def income_category_hist(df) do
+    VL.new(
+      title: [
+        text: "Income category histogram"
+      ],
+      width: 500,
+      height: 500,
+      columns: 3,
+      config: [
+        axis: [
+          grid: true,
+          grid_color: "#dedede"
+        ]
+      ]
+    )
+    |> VL.data_from_values(df)
+    |> VL.mark(:bar)
+    |> VL.encode_field(:x, "income_category", axis: [ticks: true])
+    |> VL.encode_field(:y, "value count", aggregate: :count)
     |> VL.Viewer.show()
   end
 end
