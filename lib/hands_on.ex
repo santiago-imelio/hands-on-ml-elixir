d994572315a29a179ab4e4d5245caf6473b616d2
Santiago Imelio <santiago.imelio@mimiquate.com>
2023-06-04 21:16:09 -0300

    train and evaluate using linear regression model

--- lib/hands_on.ex
+++ lib/hands_on.ex
@@ -1,20 +1,53 @@
 defmodule HandsOn do
   alias Learning.Housing
   alias Learning.Utils
-  alias Explorer.DataFrame
+  alias Explorer.DataFrame, as: DF
   alias Scholar.Linear.LinearRegression
+  alias Scholar.Metrics
 
   def housing do
-    raw_data = Housing.load_housing_data()
-    {train_data, test_data} = Utils.shuffle_and_split_data(raw_data)
-    labels = Housing.labels(train_data)
+    # fetch and load california housing dataset data
+    housing_df = Housing.load_housing_data()
 
-    IO.puts("Train data size: #{DataFrame.n_rows(train_data)}")
-    IO.puts("Test data size: #{DataFrame.n_rows(test_data)}")
+    # split and shuffle data into training and test
+    {train_data_df, test_data_df} = Utils.shuffle_and_split_data(housing_df, 700)
 
-    train_data
-    |> DataFrame.discard("median_house_value")
-    |> Housing.preprocessing()
-    |> LinearRegression.fit(labels)
+    IO.puts("Train data size: #{DF.n_rows(train_data_df)}")
+    IO.puts("Test data size: #{DF.n_rows(test_data_df)}")
+
+    # labels
+    y = Nx.concatenate(housing_df[["median_house_value"]])
+    y_train = Nx.concatenate(train_data_df[["median_house_value"]])
+    y_test = Nx.concatenate(test_data_df[["median_house_value"]])
+
+    # preprocessed training data
+    x_train =
+      train_data_df
+      |> DF.discard("median_house_value")
+      |> Housing.preprocessing()
+
+    # preprocessded test data
+    x_test =
+      test_data_df
+      |> DF.discard("median_house_value")
+      |> Housing.preprocessing()
+
+    IO.puts("training model ...")
+
+    # train linear model
+    model = LinearRegression.fit(x_train, y_train)
+
+    IO.puts("\nDone.\n")
+
+    # predict on test set
+    predictions = LinearRegression.predict(model, x_test)
+
+    # calculate errors
+    rmse = Metrics.mean_square_error(y_test, predictions) |> Nx.sqrt()
+    mae = Metrics.mean_absolute_error(y_test, predictions)
+
+    IO.puts("target mean: #{Nx.mean(y) |> Nx.to_number}")
+    IO.puts("root mean square error: #{Nx.to_number(rmse)}")
+    IO.puts("mean absolute error: #{Nx.to_number(mae)}")
   end
 end
