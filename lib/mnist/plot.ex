c23ab54678cb26d91c343ba562663186cd5b6bf4
Santiago Imelio <santiago.imelio@mimiquate.com>
2023-06-26 16:32:46 -0300

    normalized confusion matrix plot

--- lib/mnist/plot.ex
+++ lib/mnist/plot.ex
@@ -3,7 +3,7 @@ defmodule MNIST.Plot do
   alias Explorer.DataFrame, as: DF
   alias VegaLite, as: VL
 
-  def show_confusion_matrix(y_true, y_pred, num_classes \\ 10) do
+  def confusion_matrix(y_true, y_pred, num_classes \\ 10) do
     conf_matrix = Metrics.confusion_matrix(y_true, y_pred, [num_classes: num_classes])
     conf_list = Nx.to_flat_list(conf_matrix)
 
@@ -46,5 +46,61 @@ defmodule MNIST.Plot do
       |> VL.encode_field(:text, "confusion_val")
     ])
     |> VL.Viewer.show()
+  end
+
+  def normalized_confusion_matrix(y_true, y_pred, num_classes \\ 10) do
+    normalized_conf_matrix =
+      y_true
+      |> Metrics.confusion_matrix(y_pred, [num_classes: num_classes])
+      |> normalize_by_rows()
+
+    conf_list = Nx.to_flat_list(normalized_conf_matrix)
+
+    {conf_size, _cols} = Nx.shape(normalized_conf_matrix)
+
+    labels = Enum.to_list(0..9)
+
+    ncm_df =
+      DF.new(
+        true_label: List.flatten(List.duplicate(labels, conf_size)),
+        predicted_label: List.flatten(for label <- labels, do: List.duplicate(label, conf_size)),
+        confusion_ratio: conf_list
+      )
+
+    VL.new(
+      title: [
+        text: "Normalized Confusion Matrix by row"
+      ],
+      width: 500,
+      height: 500
+    )
+    |> VL.data_from_values(ncm_df)
+    |> VL.layers([
+      VL.new()
+      |> VL.mark(:rect)
+      |> VL.encode_field(:x, "predicted_label", type: :ordinal)
+      |> VL.encode_field(:y, "true_label", type: :ordinal)
+      |> VL.encode_field(:color, "confusion_ratio", [
+        type: :quantitative,
+        scale: [scheme: "turbo"]
+      ]),
+      VL.new()
+      |> VL.encode_field(:x, "predicted_label", type: :ordinal)
+      |> VL.encode_field(:y, "true_label", type: :ordinal)
+      |> VL.mark(:text, [
+        font_weight: :bold,
+        font_size: 12,
+        color: "#FFFF"
+      ])
+      |> VL.encode_field(:text, "confusion_ratio", [
+        format: ".2f",
+      ])
+    ])
+    |> VL.Viewer.show()
+  end
+
+  defp normalize_by_rows(%Nx.Tensor{shape: {rows, _cols}} = x) do
+    for i <- 0..rows - 1 do Nx.divide(x[i], Nx.sum(x[i])) end
+    |> Nx.stack(axis: 1)
   end
 end
